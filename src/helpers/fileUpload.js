3398af23eccdd52a4ed0877845ad51411b4b2126
Santiago <santic93.sc@gmail.com>
2024-09-03 09:36:07 -0300

    add images cloudinary

--- src/helpers/fileUpload.js
+++ src/helpers/fileUpload.js
@@ -1,13 +1,13 @@
 export const fileUpload = async (file) => {
   if (!file) throw new Error('No tenemos ningun archivo a subir');
-
-  const cloudUrl =
-    'https://api.cloudinary.com/v1_1/dcf2mlfo1/auto/upload';
+  const rea = 'react-journal';
+  // const cloudUrl = 'https://api.cloudinary.com/v1_1/dcf2mlfo1/auto/upload';
+  const cloudUrl = 'https://api.cloudinary.com/v1_1/dcf2mlfo1/upload';
   const formData = new FormData();
   formData.append('file', file);
-  formData.append('upload_preset', 'react-journal');
-  formData.append('public_id', 'udemy');
-  formData.append('api_key', 357383512288624);
+  formData.append('upload_preset', rea);
+  // formData.append('public_id', 'udemy');
+  // formData.append('api_key', 357383512288624);
   try {
     const resp = await fetch(cloudUrl, { method: 'POST', body: formData });
     if (!resp.ok) throw new Error('No se pudo subir archivo');
--- src/journal/components/ImageGallery.jsx
+++ src/journal/components/ImageGallery.jsx
@@ -2,19 +2,20 @@ import * as React from 'react';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 export const ImageGallery = ({ images }) => {
-
+  console.log(images)
   return (
     <ImageList sx={{ width: '100%', height: 500 }} cols={4} rowHeight={200}>
-      { images?.map((image) => (
-        <ImageListItem key={image}>
-          <img
-            src={`${image}?w=164&h=164&fit=crop&auto=format`}
-            srcSet={`${image}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-            alt="Imagen de la nota"
-            loading="lazy"
-          />
-        </ImageListItem>
-      ))}
-    </ImageList>
+    {images?.map((image, index) => (
+      <ImageListItem key={`${image}-${index}`}>
+        <img
+          src={`${image}?w=164&h=164&fit=crop&auto=format`}
+          srcSet={`${image}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+          alt='Imagen de la nota'
+          loading='lazy'
+        />
+      </ImageListItem>
+    ))}
+  </ImageList>
+  
   );
-}
+};
